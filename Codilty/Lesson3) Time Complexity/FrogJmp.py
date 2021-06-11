562d733339033c236c23c7db7caa61ba7720c56b
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-06-12 01:28:56 +0900

    Update FrogJmp.py

--- Codilty/Lesson3) Time Complexity/FrogJmp.py
+++ Codilty/Lesson3) Time Complexity/FrogJmp.py
@@ -5,3 +5,9 @@ def solution(X, Y, D):
         return jump_count
     else:
         return jump_count +1 
+# 조건부 표현식
+def solution(X, Y, D):
+    position = Y - X
+    jump_count = position // D
+    
+    return jump_count if position % D == 0 else jump_count + 1
