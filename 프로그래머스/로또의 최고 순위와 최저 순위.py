06011f3d55ed647765892db436e5c1b05ba0856d
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-07-25 18:26:17 +0900

    Add files via upload

---
+++ 프로그래머스/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,26 @@
+rank = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5}
+
+
+def solution(lottos, win_nums):
+    answer = []
+    min_count = 0
+    max_count = 0
+
+    for win_num in win_nums:
+        if win_num in lottos:
+            min_count += 1
+
+    max_count = min_count + lottos.count(0)
+
+    if max_count == 0:
+        answer = [6, 6]
+        return answer
+
+    answer.append(rank[max_count])
+
+    if min_count <= 1:
+        answer.append(6)
+    else:
+        answer.append(rank[min_count])
+
+    return answer
