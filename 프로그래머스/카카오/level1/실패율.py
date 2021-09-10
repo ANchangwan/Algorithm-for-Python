ee26ce0b1592c60d4899c0546aaa605f429fcc7d
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-09-10 23:38:22 +0900

    Create 실패율.py

---
+++ 프로그래머스/카카오/level1/실패율.py
@@ -0,0 +1,20 @@
+def solution(N, stages):
+    answer = []
+    count = 0
+    chellenge = len(stages)
+    
+    for x in range(1, N+1):
+        c = stages.count(x)
+        if c == 0:
+            fail = 0
+            
+        else:
+            fail = c / chellenge
+            chellenge -= c
+        answer.append((x, fail))
+    answer.sort(key=lambda x:x[1], reverse=True)
+    answer = [x[0] for x in answer]
+    
+            
+        
+    return answer
