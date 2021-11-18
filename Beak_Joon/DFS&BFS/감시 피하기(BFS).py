22f132ac364adf15b984c0afcc5ddecf5047f1b0
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-11-18 17:01:08 +0900

    Add files via upload

---
+++ Beak_Joon/DFS&BFS/감시 피하기(BFS).py
@@ -0,0 +1,59 @@
+from collections import deque
+from itertools import combinations as cb
+import copy
+n = int(input())
+graph = []
+teacher = []
+wall = []
+for i in range(n):
+    graph.append(list(input().split()))
+    for j in range(n):
+        if graph[i][j] == "T":
+            teacher.append((i,j))
+        if graph[i][j] == "X":
+            wall.append((i,j))
+
+
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+def bfs():
+    q = deque(teacher)
+
+    while q:
+        x, y = q.popleft()
+        temp = copy.deepcopy(graph)
+        for i in range(4):
+            _x,_y = x,y
+            while True:
+                nx = _x + dx[i]
+                ny = _y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n:
+                    if temp[nx][ny] == "O":
+                        break
+                    elif temp[nx][ny] == "S":
+                        return False
+                    elif temp[nx][ny] == "X":
+                        temp[nx][ny] = "T"
+                    _x,_y = nx, ny
+                else:
+                    break
+    return True
+
+find = False
+for data in cb(wall, 3):
+    for x,y in data:
+        graph[x][y] = "O"
+
+    if bfs():
+        find = True
+        break
+
+    for x, y in data:
+        graph[x][y] = "X"
+
+if find:
+    print("YES")
+else:
+    print("NO")
