4760d30bde1e82f2c6a278190c86be13f903d435
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2022-02-08 17:54:31 +0900

    Update 소수찾기.py

--- 프로그래머스/완전 탐색/소수찾기.py
+++ 프로그래머스/완전 탐색/소수찾기.py
@@ -3,7 +3,7 @@ from itertools import permutations as pt
 def is_prime_number(x):
     x = int(x)
     if x == 1 or x == 0:
-      return False
+        return False
     for i in range(2, x):
         if x%i == 0:
             return False
@@ -13,15 +13,15 @@ def solution(numbers):
     answer = 0
     numbers = str(numbers)
     lst = []
+    res = []
     for i in range(1, len(numbers)+2):
         temp = list(pt(numbers, i))
         lst = ["".join(p) for p in temp ]
-        for x in lst:
-            print(x)
+        for x in set(lst):
             x = x.lstrip("0")
-            if is_prime_number(x):
-                answer += 1
-  
+            if x == "":
+                continue
+            elif is_prime_number(x):
+                res.append(x)
+    answer = len(list(set(res)))
     return answer
-
-print(solution("011"))
